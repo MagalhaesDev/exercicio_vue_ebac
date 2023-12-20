878677ec87d11afd9eacc65d0742d2ce907735be
MagalhaesDev <mateusmagalhaesemidio@gmail.com>
2023-12-20 13:38:34 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,79 @@
+<!-- app.vue -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <input v-model="num1" type="number" @input="calcular" class="input-field">
+    <select v-model="operador" @change="calcular" class="select-field">
+      <option value="+">+</option>
+      <option value="-">-</option>
+      <option value="*">*</option>
+      <option value="/">/</option>
+    </select>
+    <input v-model="num2" type="number" @input="calcular" class="input-field">
+
+    <p class="resultado">Resultado: {{ resultado }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operador: '+',
+      resultado: 0
+    };
+  },
+  methods: {
+    calcular() {
+      switch (this.operador) {
+        case '+':
+          this.resultado = this.num1 + this.num2;
+          break;
+        case '-':
+          this.resultado = this.num1 - this.num2;
+          break;
+        case '*':
+          this.resultado = this.num1 * this.num2;
+          break;
+        case '/':
+          this.resultado = this.num1 / this.num2;
+          break;
+        default:
+          this.resultado = 0;
+      }
+    }
   }
-}
+};
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  max-width: 400px;
+  margin: 50px auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  background-color: #f0f0f0;
+}
+
+.input-field, .select-field, .resultado {
+  margin-bottom: 10px;
+}
+
+.input-field, .select-field {
+  padding: 8px;
+  border: 1px solid #ddd;
+  border-radius: 3px;
+}
+
+.select-field {
+  width: 100%;
+}
+
+.resultado {
+  font-weight: bold;
+  color: #333;
 }
 </style>
